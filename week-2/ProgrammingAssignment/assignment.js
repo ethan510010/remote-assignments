039b0fa6c296ad66f8ca94dde3df7463a0e373e5
EthanLin <ethan510010@gmail.com>
2019-11-11 20:49:09 +0800

    作業1補上測試結果

--- week-2/ProgrammingAssignment/assignment.js
+++ week-2/ProgrammingAssignment/assignment.js
@@ -1,5 +1,6 @@
 /* Assignment 1: Function and Array
 Complete the function below to find the ​max number​ of passing numbers. You may refer to this ​MDN document​ about rest parameters of the function. */
+
 function max(...numbers) {
     let resultArray = [numbers[0]];
     for (let i = 0; i < numbers.length; i++) {
@@ -12,3 +13,6 @@ function max(...numbers) {
     }
     console.log(resultArray[resultArray.length-1]);
 }
+
+max(1, 2, 4, 5);
+max(5, 2, 7, 1, 6);
