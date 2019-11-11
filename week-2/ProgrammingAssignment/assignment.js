8e7644f71b12fc4c71c35e1e3df3606ce4c76df8
EthanLin <ethan510010@gmail.com>
2019-11-11 20:47:07 +0800

    Assignment1: find max number completed

---
+++ week-2/ProgrammingAssignment/assignment.js
@@ -0,0 +1,14 @@
+/* Assignment 1: Function and Array
+Complete the function below to find the ​max number​ of passing numbers. You may refer to this ​MDN document​ about rest parameters of the function. */
+function max(...numbers) {
+    let resultArray = [numbers[0]];
+    for (let i = 0; i < numbers.length; i++) {
+        if (i < numbers.length) {
+            let resultLastItem = resultArray[resultArray.length-1];
+            if (numbers[i+1] >= resultLastItem) {
+                resultArray.push(numbers[i+1])
+            }
+        }
+    }
+    console.log(resultArray[resultArray.length-1]);
+}
