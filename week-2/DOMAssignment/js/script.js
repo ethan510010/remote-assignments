c688a12d6118001e6c618bcb1501f2cd38120b42
EthanLin <ethan510010@gmail.com>
2019-11-13 14:00:33 +0800

    出現隱藏區塊完成

--- week-2/DOMAssignment/js/script.js
+++ week-2/DOMAssignment/js/script.js
@@ -4,3 +4,16 @@ banner.addEventListener('click', () => {
     const bannerTitleElement = document.querySelector(".banner h1")
     bannerTitleElement.textContent = "Have a Good Time!";
 })
+
+let isShowBoxes2 = false;
+
+const actionButton = document.querySelector(".boxes-1 .call-action")
+actionButton.addEventListener('click', () => {
+    isShowBoxes2 = !isShowBoxes2;
+    const boxes2 = document.querySelector(".boxes-2")
+    if (isShowBoxes2) {
+        boxes2.style.display = "flex";
+    } else {
+        boxes2.style.display = "none";
+    }
+})
