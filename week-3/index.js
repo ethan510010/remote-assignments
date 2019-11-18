251ebecff71fc04063f2c8438e5c88e66f4ab399
EthanLin <ethan510010@gmail.com>
2019-11-18 23:16:18 +0800

    Assignment2完成

--- week-3/index.js
+++ week-3/index.js
@@ -6,5 +6,22 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello, My Server!</h1>")
 })
 
+// Assignment2: Build Backend API for Front-End
+app.get('/getData', (req, res) => {
+    let resMessage = ""
+    if (req.query.number) {
+        if (parseInt(req.query.number)) {
+            let validNumber = parseInt(req.query.number)
+            let sum = (1 + validNumber) * validNumber / 2
+            resMessage = `${sum}`
+        } else {
+            resMessage = "Wrong Parameter"
+        }
+    } else {
+        resMessage = "Lack of Parameter"
+    }
+    res.send(resMessage)
+})
+
 app.listen(3000)
 
